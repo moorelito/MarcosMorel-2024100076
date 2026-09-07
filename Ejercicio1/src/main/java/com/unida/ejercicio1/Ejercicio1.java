821799c60d10c1ef7889482b2e4ceba84cc293e5
Laboratorio Asu <laboratorioasu@unida.edu.py>
2026-09-07 20:29:45 -0300

    primer commit

---
+++ Ejercicio1/src/main/java/com/unida/ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unida.ejercicio1;
+
+/**
+ *
+ * @author laboratorioasu
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
